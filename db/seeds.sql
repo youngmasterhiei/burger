9eef4e014a03e394fb608e59bba506c04dc8220b
Hiei <james.illius@yahoo.com>
2018-07-31 17:31:59 -0400

    asdf

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (name) VALUES ('cheese');
+INSERT INTO burgers (name) VALUES ('pizza');
+INSERT INTO burgers (name, devoured) VALUES ('cheese', true);
+INSERT INTO burgers (name, sleepy) VALUES ('bacon', true);
+INSERT INTO burgers (name) VALUES ('blue');
